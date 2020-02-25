616902d36d45e732b48cd9d2062d0bbac44a926c
anandsimmy <anandsimmy7@gmail.com>
2020-02-25 05:41:51 -0500

    1 file modified

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import "./style.css";a
+import "./style.css";
 import axios from 'axios'
 
 function getCookie(cname) {
